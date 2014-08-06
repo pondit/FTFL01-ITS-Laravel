03218a035cfc1e39553e42f12ccd898ef40fe4e7
Towfiqur Rahman <towfiqpiash@gmail.com>
2014-08-06 23:34:31 +0600

    Simple jQuery Plugin

---
+++ ITS_2014000057_Towfiqur_Rahman/jQuery/jqueryPlugins/js/jquery.hello-world.js
@@ -0,0 +1,26 @@
+(function($) {
+
+    $.fn.helloWorld = function(options) {
+
+    	//Establish our default settings
+    	var settings = $.extend({
+    		text : 'Hello World!',
+    		color: null,
+    		fontStyle: null
+    	}, options);
+
+        return this.each( function() {
+            $(this).text(settings.text);
+
+            if(settings.color){
+            	$(this).css('color',settings.color);
+            }
+
+            if(settings.fontStyle){
+            	$(this).css('font-style',settings.fontStyle);
+            }
+        });
+
+    }
+
+}(jQuery));
