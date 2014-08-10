ef86d961a325149f896842d54e4306183bb966de
towfiqpiash <towfiqpiash@gmail.com>
2014-08-10 12:25:22 +0600

    Multiple table join

--- ITS_2014000057_Towfiqur_Rahman/database/ftfl_exercise.sql
+++ ITS_2014000057_Towfiqur_Rahman/database/ftfl_exercise.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2014 at 05:40 AM
+-- Generation Time: Aug 10, 2014 at 08:23 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -19,6 +19,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `ftfl_exercise`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `title`, `code`, `created`, `modified`) VALUES
+(1, 'LARAVEL', 'P-002', '2014-08-10 11:42:30', '2014-08-10 11:42:30'),
+(2, 'Photoshop', 'D-003', '2014-08-10 11:42:30', '2014-08-10 11:42:30'),
+(3, 'Unity 3D', 'G-004', '2014-08-10 11:42:53', '2014-08-10 11:42:53');
 
 -- --------------------------------------------------------
 
@@ -59,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `results` (
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   KEY `student_id` (`student_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `results`
@@ -68,7 +92,8 @@ CREATE TABLE IF NOT EXISTS `results` (
 INSERT INTO `results` (`id`, `student_id`, `exam_level`, `gpa`, `created`, `modified`) VALUES
 (1, 11, 'SSC', 5, '2014-08-10 09:20:43', '2014-08-10 09:20:43'),
 (2, 11, 'HSC', 3.5, '2014-08-10 09:20:43', '2014-08-10 09:20:43'),
-(3, 11, 'Ba', 4, '2014-08-10 09:21:39', '2014-08-10 09:21:39');
+(3, 11, 'Ba', 4, '2014-08-10 09:21:39', '2014-08-10 09:21:39'),
+(6, 12, 'HSC', 4.5, '2014-08-10 09:49:51', '2014-08-10 09:49:20');
 
 -- --------------------------------------------------------
 
@@ -89,8 +114,36 @@ CREATE TABLE IF NOT EXISTS `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `created`, `modified`) VALUES
+(1, 'Sajia', '2014-08-10 11:57:27', '2014-08-10 11:57:27'),
+(2, 'Tanjim', '2014-08-10 11:57:49', '2014-08-10 11:57:49'),
 (11, 'Sajia', '2014-08-10 09:12:57', '2014-08-10 09:12:57'),
 (12, 'Daisy', '2014-08-10 09:12:57', '2014-08-10 09:12:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_courses`
+--
+
+CREATE TABLE IF NOT EXISTS `students_courses` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `student_id` int(255) NOT NULL,
+  `course_id` int(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `student_id` (`student_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `students_courses`
+--
+
+INSERT INTO `students_courses` (`id`, `student_id`, `course_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1),
+(4, 2, 2),
+(5, 2, 3);
 
 --
 -- Constraints for dumped tables
@@ -107,6 +160,13 @@ ALTER TABLE `final_results`
 --
 ALTER TABLE `results`
   ADD CONSTRAINT `results_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
+
+--
+-- Constraints for table `students_courses`
+--
+ALTER TABLE `students_courses`
+  ADD CONSTRAINT `students_courses_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  ADD CONSTRAINT `students_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
